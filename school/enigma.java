431717b447e65df7b63614e399e7523e465c95c1
rymo90 <red01.yassin@gmail.com>
2016-12-22 11:31:07 +0100

    javaforpythonprogrammerare
    it’s about character match basically

--- school/enigma.java
+++ school/enigma.java
@@ -4,7 +4,10 @@ public class enigma{
 
   public static void main(String[] args) {
     Scanner input = new Scanner (System.in);
-    String kryptext;
-    String klartext;
+    String kryptext = input.next();
+    String klartext = input.next();
+    
+
+
   }
 }
