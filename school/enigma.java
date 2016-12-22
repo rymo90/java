46a47106d850b315ab942d1e06041327aa63fae1
rymo90 <red01.yassin@gmail.com>
2016-12-22 03:52:18 +0100

    implmented input scann
    funs tuff

--- school/enigma.java
+++ school/enigma.java
@@ -1,8 +1,8 @@
 import java.uitl.Scanner;
 
 public class enigma{
-  
-  public static void main(String[] args) {
 
+  public static void main(String[] args) {
+    Scanner input = new Scanner (System.in);
   }
 }
