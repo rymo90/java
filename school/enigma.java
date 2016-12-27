d34db3be4521850517ea13a2202e453fda295402
rymo90 <red01.yassin@gmail.com>
2016-12-27 04:26:10 +0100

    enigma
    almost there !
    hava to look a couple of example

--- school/enigma.java
+++ school/enigma.java
@@ -1,28 +1,37 @@
 import java.util.Scanner;
-import java.lang.Math;
+import java.util.ArrayList;
 
 public class enigma{
 
-  public int matchingIndex(String kryptext, String klartext){
-  char [] first = kryptext.toLowerCase().toCharArray();
-  char [] second = klartext.toLowerCase().toCharArray();
-  int counter = 0;
-  int diff = Math.min(first.length, second.length);
-  for (int i = 0; i < diff ;i++ ) {
-    if (first[i]!= second[i]) {
-      counter++;
+  public  static int[] indexOf(String kryptext, String klartext){
+  int diff = kryptext.length()- klartext.length();
+  int[] counter = new int[0];
+  for (int i = 0 ;i<= diff ;i++ ) {
+    for (int j = 0; j< klartext.length() ;j++ ) {
+      if (kryptext.charAt(i+j) != klartext.charAt(j)) {
+        counter[j] = i;
+      }
+      else {
+        break;
       }
     }
-    return counter;
-
   }
+  return counter;
+}
+
 
   public static void main(String[] args) {
     Scanner input = new Scanner (System.in);
     String kryptext = input.next();
     String klartext = input.next();
     enigma start = new enigma();
-    int svar = start.matchingIndex(kryptext, klartext);
-    System.out.println(svar);
-  }
+    start.indexOf(kryptext,klartext);
+    int svar[] = start.indexOf(kryptext, klartext);
+    for (int l = 0; l < svar.length ;l++ ) {
+      System.out.print(' ');
+      System.out.print(svar[l] + " ");
+    }
+
+    }
+
 }
